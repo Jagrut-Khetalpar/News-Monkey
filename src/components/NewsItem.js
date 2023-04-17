78f2d1d14f7637ad2d6b86e73311e23e4846eca5
Jagrut <khetalparjagrut@gmail.com>
2023-04-17 14:55:21 +0530

    Initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://techcrunch.com/wp-content/uploads/2022/09/07edb091bb329145ee3f3f01476adb40acc6457b.jpg?resize=1200,675"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+              <span
+                className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+                style={{ left: "90%", zIndex: "1" }}
+              >
+                {source}
+                <span className="visually-hidden">unread messages</span>
+              </span>
+            </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
